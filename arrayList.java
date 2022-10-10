52328dd332ac82ca94c00a3bdc7a1a62d25e07c2
deim <evanjoelsanchez@gmail.com>
2022-10-10 10:24:34 -0700

    Create arrayList.java

---
+++ arrayList.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ghArray {
+    
+    public static void main(String[] args) {
+
+        ArrayList<String> ghArray = new ArrayList<>(); // create new array list
+        Scanner sc = new Scanner(System.in); // create new scanner object
+
+        String addNewUser = "y";
+
+        while (addNewUser.equals("y")) { // as long as user wants to add a new user, repeat
+            System.out.println("Enter your name -");
+            ghArray.add("Name: " + sc.nextLine()); // take name input
+    
+            System.out.println("Enter your Github ID -");
+            ghArray.add("Github ID: " + sc.nextLine()); // take ghID input
+        
+            System.out.println("Would you like to input another user? (y/n)");
+            addNewUser = sc.nextLine(); // input y/n to add another user
+        }
+
+        System.out.println();
+        System.out.println("================================="); // new line
+
+        for (int num = 0; num < ghArray.size(); num +=2){ // print every 2 indexes and leave a space
+            System.out.println(ghArray.get(num));
+            System.out.println(ghArray.get(num+1));
+            System.out.println();
+        }
+    }
+}
+
+ghArray.main(null);
