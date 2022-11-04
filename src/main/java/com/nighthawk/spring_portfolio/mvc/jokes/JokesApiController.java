51d090ed47d60a60522cd2667c2db06da0066683
Samuelwaang <samuelwaang@gmail.com>
2022-11-03 23:16:34 -0700

    test

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesApiController.java
@@ -78,7 +78,7 @@ public class JokesApiController {
     public ResponseEntity<Jokes> getNewJoke(@PathVariable String jokeStr,@PathVariable String anthorStr, @PathVariable String img) {
 
         // return ResponseEntity.ok( questionBankService.getSpecificquestions(subject,topic));
-        Jokes input = new Jokes(null, jokeStr, anthorStr,img,"id", "0");
+        Jokes input = new Jokes(null, jokeStr, anthorStr, img, "id", "0");
         repository.save(input);
         return new ResponseEntity<>(input, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
 
