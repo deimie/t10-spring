a087df17da147a965c1ada50a0f30f261abeefc1
Samuelwaang <samuelwaang@gmail.com>
2022-11-03 23:38:34 -0700

    test

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesJpaRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
+// JPA is an obect-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
 public interface JokesJpaRepository extends JpaRepository<Jokes, Long> {
     // JPA has many built in methods, these few have been prototyped for this application
     void save(String Joke);
